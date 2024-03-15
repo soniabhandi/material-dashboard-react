f957b278b43075514819d90d6a5eb93a827ea0c1
soniabhandi <soniabhandi@gmail.com>
2024-03-15 20:12:15 +0530

    added buttons

--- src/layouts/prefilled-data/index.js
+++ src/layouts/prefilled-data/index.js
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import * as React from "react";
+import MDButton from "components/MDButton";
+import TextField from "@mui/material/TextField";
 
 // @mui material components
 import Grid from "@mui/material/Grid";
@@ -6,7 +9,7 @@ import Grid from "@mui/material/Grid";
 // Material Dashboard 2 React components
 import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
-
+import Button from "@mui/material/Button";
 // Material Dashboard 2 React example components
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 
@@ -15,7 +18,13 @@ function PrefilledData() {
     <DashboardLayout>
       <MDBox mt={6} mb={3}>
         <Grid container spacing={3} justifyContent="center">
-          <Grid item xs={12} lg={8}></Grid>
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+
+          <Grid item xs={12} lg={8}>
+            <MDButton variant="gradient" color="info">
+              Submit
+            </MDButton>
+          </Grid>
         </Grid>
       </MDBox>
     </DashboardLayout>
